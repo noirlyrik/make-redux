0f67e72c698126c3af4f3c92958f4182e8bf5dd2
Yopu <yopu@YopudeiMac.lan>
2020-09-08 09:41:09 +0800

    redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+let appState ={
+  title: {
+    text:  'react',
+    color: 'blue'
+  }, 
+  content:{
+    text: 'js',
+    color: 'red'
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function createStore(state, reducer) {
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach((listener)=>listener())
+  }
+  return { getState, dispatch, subscribe }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function stateChanger(state, action) {
+  switch (action.type){
+    case 'UPDATE_TITLE_TEXT':
+      return {        
+           ...state,
+          title: {
+            ...state.title,
+            text: action.text
+          }          
+      }
+    case 'UPDATE_TITLE_COLOR':
+      return {
+        ...state,
+        title: { ...state.title, color: action.color}
+      }
+    default:
+      return state
+  }
+}
+
+function renderApp(appState, oldState={}) {
+  if(appState === oldState) return 
+  console.log('render app...')
+  renderTitle(appState.title, oldState.title)
+  renderContent(appState.content, oldState.content)
+}
+
+function renderTitle(title, oldTitle={}){
+  if(title === oldTitle) return 
+  console.log('render title...')
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent(content, oldContent={}){
+  if(content === oldContent) return 
+  console.log('render content...')
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+const store = createStore(appState, stateChanger)
+let oldState = store.getState()
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+})
+
+renderApp(store.getState())
+store.dispatch({type: 'UPDATE_TITLE_TEXT', text: 'reactjs小书'})
+store.dispatch({type: 'UPDATE_TITLE_COLOR', color: 'black'})
